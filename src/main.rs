a07b9f2023ab87dcdb9954db4ece1a3748f7b1af
CNife <CNife@vip.qq.com>
2020-05-24 15:25:01 +0800

    完成功能

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+use std::io::Write;
+
+const MAX_DIGITS: usize = 8;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Convert binary number to decimal.");
+
+    let mut buffer = String::new();
+    loop {
+        print!("Input: ");
+        io::stdout().flush().expect("Error when flushing stdout");
+
+        buffer.clear();
+        match io::stdin().read_line(&mut buffer) {
+            Ok(len) => {
+                if len - 1 > MAX_DIGITS {
+                    eprintln!("Too Long, 8 digits at most");
+                    continue;
+                }
+
+                match u8::from_str_radix(&buffer.trim_end(), 2) {
+                    Ok(result) => println!("{}", result),
+                    Err(parse_err) => eprintln!("Error when parsing: {:?}", parse_err),
+                }
+            }
+            Err(err) => {
+                eprintln!("Error when reading stdin: {:?}", err)
+            }
+        }
+    }
 }
