a2159614d2f4f56561cd3df7dfa610583953cc3b
CNife <CNife@vip.qq.com>
2020-05-24 17:33:39 +0800

    添加了允许转换任意长度二进制数字的功能

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
-//! # Bin2Dec - 二进制数转十进制
-//! 
-//! 这是 [app-ideas/Bin2Dec](https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Bin2Dec-App.md) 的一个 Rust 实现。
-//! 
-//! ## 用法
-//! `cargo run` 编译并运行程序，每行输入一个二进制数字，按 ENTER 键转换数字。
-//! 
-//! ## 错误
-//! 1. 输入除 1 和 0 以外的字符；
-//! 2. 输入超过 8 个数字；
-//! 3. 没有输入；
-
 use std::io;
 use std::io::Write;
 
+use num::Num;
+use num_bigint::BigUint;
+
 fn main() -> io::Result<()> {
-    println!("bin2dec");
+    println!("bin2dec - 二进制转十进制");
 
     let mut buffer = String::new();
     loop {
@@ -28,14 +19,38 @@ fn main() -> io::Result<()> {
         // 需要注意的是，即使不用去掉首尾空格，也不能简单地用 &buffer[..buffer.len() - 1]，
         // 因为 Windows 下换行符是 CRLF，是两个字符
         let digits = buffer.trim();
-        if digits.len() > 8 {
-            eprintln!("input too long , 8 digits at most");
+
+        if digits.is_empty() {
             continue;
         }
 
-        match u8::from_str_radix(digits, 2) {
-            Ok(result) => println!("{} => {}", digits, result),
-            Err(err) => eprintln!("input is not valid\n\t{:?}", err),
+        // 检查输入中是否包含非法字符
+        if let Err(index) = check_input(digits) {
+            print_invalid_char_message(digits, index);
+            continue;
+        }
+
+        match BigUint::from_str_radix(digits, 2) {
+            Ok(result) => println!("{}", result),
+            Err(err) => eprintln!("转换失败：{:?}", err),
         }
     }
+}
+
+fn check_input(input: &str) -> Result<(), usize> {
+    for (i, ch) in input.chars().enumerate() {
+        if ch != '0' && ch != '1' {
+            return Err(i);
+        }
+    }
+    Ok(())
+}
+
+fn print_invalid_char_message(input: &str, index: usize) {
+    eprintln!("输入了非法字符：");
+    eprintln!("{}", input);
+    for _ in 0..index {
+        eprint!(" ");
+    }
+    eprintln!("^");
 }
